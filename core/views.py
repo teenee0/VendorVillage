1546a099dd2a40ae32cd1f61120df63447f57e4f
teenee0 <qwerty.gogi@mail.ru>
2025-02-22 16:23:39 +0300

    ltl fx

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -25,8 +26,13 @@ def business_site(request, slug):
     print(business.html_template)
     if business.html_template:
         print(business.html_template)
+        page_number = request.GET.get('page', 1)
+        paginator = Paginator(products, 12)  # 12 товаров на страницу
+        page_obj = paginator.get_page(page_number)
         template = business.html_template
-        return render(request, str(template), {'business': business, 'products': products})
+        return render(request, str(template),
+                      {'business': business,
+                       'page_obj': page_obj,})
     return render(request, 'business_defaults/business_default_page.html', {'business': business, 'products': products})
 
 
